c2c99609d29ad39866c59bdb0320663b8c7f54bf
MariiaHula <mariyagulaya@gmail.com>
2023-11-03 14:01:08 +0200

    ашч іендуі

--- src/pages/MovieDetails/MovieDtails.styled.js
+++ src/pages/MovieDetails/MovieDtails.styled.js
@@ -49,7 +49,7 @@ export const GenreItem = styled.li`
 
 export const AdditionalInfo = styled.div`
   margin-top: 20px;
-  padding: 10px;
+  padding: 20px;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px,
     rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 48px;
 `;
