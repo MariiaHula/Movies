7870b94dc538302031ecd09192a7b9778c54718d
MariiaHula <mariyagulaya@gmail.com>
2023-11-03 13:02:00 +0200

    fix styles

--- src/pages/MovieDetails/MovieDtails.styled.js
+++ src/pages/MovieDetails/MovieDtails.styled.js
@@ -8,7 +8,7 @@ export const MovieDetailsContainer = styled.div`
 `;
 
 export const MovieInfo = styled.div`
-  padding: 10px;
+  padding: 20px;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px,
     rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 48px;
   display: flex;
@@ -25,6 +25,7 @@ export const MoviePoster = styled.img`
 `;
 
 export const MovieTitle = styled.h1`
+  margin-bottom: 20px;
   font-size: 24px;
 `;
 
